24dba8b936e01bfe4e787b129086d64360ecaa18
imdongyoung <imdong207@gmail.com>
2021-12-09 22:44:29 +0900

    init quiz project

---
+++ app.js
@@ -0,0 +1,113 @@
+import { question } from './questionJson.js';
+
+let getQuestion = [];
+let missQuestion = [];
+let spliceQuestion;
+let QuestionCount = 1;
+let getQuestionCount = 0;
+let MissQuestionCount = 0;
+let remaining = question.length;
+
+const $questionForm = document.querySelector('.question');
+const $input = document.querySelector('.input');
+const $next = document.querySelector('.next');
+const $getQuestionCount = document.querySelector('.getQuestionCount');
+const $MissQuestionCount = document.querySelector('.MissQuestionCount');
+const $remaining = document.querySelector('.remaining');
+const $clear = document.querySelector('.clear');
+const $clearScore1 = document.querySelector('.clearScore1>p');
+const $clearScore2 = document.querySelector('.clearScore2>p');
+const $clearScore1Ul = document.querySelector('.clearScore1>ul');
+const $clearScore2Ul = document.querySelector('.clearScore2>ul');
+
+let randomQuestion = Math.floor(Math.random() * question.length);
+
+const changeQuestion = () => {
+  if (question.length <= 0) {
+    $clear.style.display = 'flex';
+    document.body.style.backgroundColor = 'rgba(0,0,0,0.4)';
+    $clearScore1.innerText = `맞힌 문제 : ${getQuestion.length}개`;
+    getQuestion.map((i) => {
+      const li = document.createElement('li');
+      $clearScore1Ul.appendChild(li).innerText = i[0].question;
+    });
+    $clearScore2.innerText = `틀린 문제 : ${missQuestion.length}개`;
+    missQuestion.map((i) => {
+      const li = document.createElement('li');
+      $clearScore2Ul.appendChild(li).innerText = i[0].question;
+    });
+    document.body.addEventListener('click', () => {
+      location.reload();
+    });
+    return;
+  }
+  randomQuestion = Math.floor(Math.random() * question.length);
+  $questionForm.innerText = `${QuestionCount}번문제) ${question[randomQuestion].question}`;
+  remaining = question.length;
+  $remaining.innerText = `남은 문제 : ${remaining}`;
+};
+
+const printQuestion = (result) => {
+  if (result == 1) {
+    spliceQuestion = question.splice(randomQuestion, 1);
+    getQuestion.push(spliceQuestion);
+  } else {
+    spliceQuestion = question.splice(randomQuestion, 1);
+    missQuestion.push(spliceQuestion);
+  }
+  changeQuestion();
+};
+
+const getQuestionEffect = () => {
+  $input.style.border = 'none';
+  $questionForm.style.color = '#000';
+  QuestionCount++;
+  printQuestion(1);
+};
+
+const missQuestionEffect = () => {
+  $input.style.border = 'none';
+  $questionForm.style.color = '#000';
+  QuestionCount++;
+  printQuestion(0);
+};
+
+const checkQuestion = () => {
+  if ($input.value === question[randomQuestion].answer) {
+    $input.style.border = '3px solid #33e99d';
+    getQuestionCount++;
+    $getQuestionCount.innerText = `맞힌 문제 : ${getQuestionCount}`;
+    $input.value = '';
+    $questionForm.innerText = '맞았습니다!';
+    $questionForm.style.color = '#33e99d';
+    setTimeout(getQuestionEffect, 1000);
+  } else {
+    $input.style.border = '3px solid #ff775f';
+    $input.value = '';
+    MissQuestionCount++;
+    $MissQuestionCount.innerText = `틀린 문제 : ${MissQuestionCount}`;
+    $questionForm.innerHTML = `틀렸습니다  <span style="color: #000;">정답 : ${question[randomQuestion].answer}</span>`;
+    $questionForm.style.color = '#ff775f';
+    setTimeout(missQuestionEffect, 2000);
+  }
+};
+
+$questionForm.innerText = `${QuestionCount}번문제) ${question[randomQuestion].question}`;
+$getQuestionCount.innerText = `맞힌 문제 : ${getQuestionCount}`;
+$MissQuestionCount.innerText = `틀린 문제 : ${MissQuestionCount}`;
+$remaining.innerText = `남은 문제 : ${remaining}`;
+$next.addEventListener('click', () => {
+  if ($input.value == '') {
+    alert('답을 입력해주세요');
+  }
+  checkQuestion();
+});
+$input.addEventListener('keypress', (event) => {
+  if (event.key == 'Enter') {
+    if ($input.value == '') {
+      alert('답을 입력해주세요');
+      return;
+    }
+    checkQuestion();
+  }
+});
